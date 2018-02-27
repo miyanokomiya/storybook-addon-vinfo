1208b273b0c3aa354ae40eb6ba8e94493ee1e11b
miyanokomiya <komiyamatomoya@mbp-2.local>
2018-02-28 08:32:39 +0900

    fix type

--- example/.storybook/config.js
+++ example/.storybook/config.js
@@ -1,15 +1,15 @@
 import { configure } from '@storybook/vue'
 import { storiesOf } from '@storybook/vue'
-import cinfo, { configure as cinfoConfigure } from 'storybook-addon-vinfo'
+import vinfo, { configure as vinfoConfigure } from 'storybook-addon-vinfo'
 
-cinfoConfigure.root = 'src/components'
-cinfoConfigure.name = 'vinfo'
+vinfoConfigure.root = 'src/components'
+vinfoConfigure.name = 'vinfo'
 const reqVue = require.context('../src/components', true, /\.vue$/)
 const addStories = () => {
   reqVue.keys().forEach(filename => {
-    storiesOf(cinfoConfigure.name + filename.slice(1), module).add(
+    storiesOf(vinfoConfigure.name + filename.slice(1), module).add(
       '_vinfo',
-      cinfo(reqVue(filename).default)
+      vinfo(reqVue(filename).default)
     )
   })
 }
