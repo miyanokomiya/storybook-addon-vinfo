8d69ea7a5fe61b38bf073b0141c84390c96e1f51
miyanokomiya <komiyamatomoya@mbp-2.local>
2018-02-28 21:28:48 +0900

    improve example

--- example/.storybook/config.js
+++ example/.storybook/config.js
@@ -4,6 +4,9 @@ import vinfo, { configure as vinfoConfigure } from 'storybook-addon-vinfo'
 
 vinfoConfigure.root = 'src/components'
 vinfoConfigure.name = 'vinfo'
+vinfoConfigure.alias = {
+  '@': 'src'
+}
 const reqVue = require.context('../src/components', true, /\.vue$/)
 const addStories = () => {
   reqVue.keys().forEach(filename => {
